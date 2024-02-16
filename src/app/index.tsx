5d6a152f405d202312ee47f1b4d60c64fe741646
Deibson Cogo <deibsoncogo@outlook.com>
2024-02-16 15:42:14 -0300

    ✨ feat: criação dos primeiros arquivos personalizado

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+export default function Home() {
+  return (
+    <View className="flex-1 pt-8">
+      <Text className="text-2xl text-white">
+        Hello React Native
+      </Text>
+    </View>
+  )
+}
