485c2aef6e15a8fba537a4f2530616a75c4fc5c0
Deibson Cogo <deibsoncogo@outlook.com>
2024-02-16 18:27:51 -0300

    🚧 feat: criação parcial das pages layout e index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,35 @@
-import { Text, View } from "react-native"
+import { useState } from "react"
+import { FlatList, View } from "react-native"
+import { CategoryButton } from "../components/category-button"
+import { Header } from "../components/header"
+import { CATEGORIES } from "../utils/data/products"
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory)
+  }
+
   return (
     <View className="flex-1 pt-8">
-      <Text className="text-2xl text-white">
-        Hello React Native
-      </Text>
+      <Header title="Faça seu pedido" cartQuantityItems={5} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+        className="max-h-10 mt-5"
+      />
     </View>
   )
 }
