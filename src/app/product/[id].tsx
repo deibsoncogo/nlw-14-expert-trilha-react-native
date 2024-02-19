4c0a95318a2aaf1ca0519ba9f7e9ac7fcad3a0d3
Deibson Cogo <deibsoncogo@outlook.com>
2024-02-19 11:20:54 -0300

    ✨ feat: implementação de ajustes

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,6 +1,6 @@
 import { Feather } from "@expo/vector-icons"
 import { Image, Text, View } from "react-native"
-import { useLocalSearchParams, useNavigation } from "expo-router"
+import { Redirect, useLocalSearchParams, useNavigation } from "expo-router"
 import { Button } from "@/src/components/button"
 import { LinkButton } from "@/src/components/link-button"
 import { useCartStore } from "@/src/stores/cart-store"
@@ -12,18 +12,24 @@ export default function Product() {
   const { id } = useLocalSearchParams()
   const cartStore = useCartStore()
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0]
+  const product = PRODUCTS.find((item) => item.id === id)
 
   function handleAddToCart() {
-    cartStore.add(product)
+    cartStore.add(product!)
     navigation.goBack()
   }
+
+  if (!product) return <Redirect href="/" />
 
   return (
     <View className="flex-1">
       <Image source={product.cover} resizeMode="cover" className="w-full h-52" />
 
       <View className="p-5 mt-8 flex-1">
+        <Text className="text-white text-xl font-heading">
+          {product.title}
+        </Text>
+
         <Text className="text-lime-400 text-2xl font-heading my-2">
           {formatCurrency(product.price)}
         </Text>
