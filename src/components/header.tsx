73bf8add6e7dd221da5eb0dfd6bc21487a165410
Deibson Cogo <deibsoncogo@outlook.com>
2024-02-19 16:58:28 -0300

    ✨ feat: criação final do projeto

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import { Feather } from "@expo/vector-icons"
 import { Image, Text, TouchableOpacity, View } from "react-native"
+import { Link } from "expo-router"
 import colors from "tailwindcss/colors"
 
 type HeaderProps = {
@@ -19,15 +20,17 @@ export function Header({ title, cartQuantityItems = 0 }: HeaderProps) {
       </View>
 
       {cartQuantityItems > 0 && (
-        <TouchableOpacity activeOpacity={0.7} className="relative">
-          <View className="bg-lime-300 w-4 h-4 rounded-full items-center justify-center top-2 z-10 -right-3.5">
-            <Text className="text-slate-900 font-bold text-xs">
-              {cartQuantityItems}
-            </Text>
-          </View>
+        <Link asChild href="/cart">
+          <TouchableOpacity activeOpacity={0.7} className="relative">
+            <View className="bg-lime-300 w-4 h-4 rounded-full items-center justify-center top-2 z-10 -right-3.5">
+              <Text className="text-slate-900 font-bold text-xs">
+                {cartQuantityItems}
+              </Text>
+            </View>
 
-          <Feather name="shopping-bag" color={colors.white} size={24} />
-        </TouchableOpacity>
+            <Feather name="shopping-bag" color={colors.white} size={24} />
+          </TouchableOpacity>
+        </Link>
       )}
     </View>
   )
--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,4 +1,6 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
 import { create } from "zustand"
+import { createJSONStorage, persist } from "zustand/middleware"
 import { ProductProps } from "../utils/data/products"
 import * as cartInMemory from "./helpers/cart-in-memory"
 
@@ -9,12 +11,25 @@ export type ProductCartProps = ProductProps & {
 type StateProps = {
   products: ProductCartProps[]
   add: (product: ProductProps) => void
+  remove: (productId: string) => void
+  clear: () => void
 }
 
-export const useCartStore = create<StateProps>((set) => ({
+export const useCartStore = create(persist<StateProps>((set) => ({
   products: [],
 
   add: (product: ProductProps) => {
     set((state) => ({ products: cartInMemory.add(state.products, product) }))
   },
+
+  remove: (productId: string) => {
+    set((state) => ({ products: cartInMemory.remove(state.products, productId) }))
+  },
+
+  clear: () => {
+    set(() => ({ products: [] }))
+  },
+}), {
+  name: "nlw-14-expert-trilha-react-native:cart",
+  storage: createJSONStorage(() => AsyncStorage),
 }))
