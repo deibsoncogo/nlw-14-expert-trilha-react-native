bd8d828713cb3eeb98c8d0f41a0a709441b971e8
Deibson Cogo <deibsoncogo@outlook.com>
2024-02-19 21:33:25 -0300

    📱 feat: implementação da rolagem de tela

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,5 +1,5 @@
 import { Feather } from "@expo/vector-icons"
-import { Image, Text, View } from "react-native"
+import { Image, ScrollView, Text, View } from "react-native"
 import { Redirect, useLocalSearchParams, useNavigation } from "expo-router"
 import { Button } from "@/src/components/button"
 import { LinkButton } from "@/src/components/link-button"
@@ -26,23 +26,25 @@ export default function Product() {
       <Image source={product.cover} resizeMode="cover" className="w-full h-52" />
 
       <View className="p-5 mt-8 flex-1">
-        <Text className="text-white text-xl font-heading">
-          {product.title}
-        </Text>
-
-        <Text className="text-lime-400 text-2xl font-heading my-2">
-          {formatCurrency(product.price)}
-        </Text>
-
-        <Text className="text-slate-400 font-body text-base leading-6 mb-6">
-          {product.description}
-        </Text>
-
-        {product.ingredients.map((ingredient) => (
-          <Text key={ingredient} className="text-slate-400 font-body text-base leading-6">
-            {"\u2022"} {" "} {ingredient}
+        <ScrollView>
+          <Text numberOfLines={1} className="text-white text-xl font-heading">
+            {product.title}
           </Text>
-        ))}
+
+          <Text className="text-lime-400 text-2xl font-heading my-2">
+            {formatCurrency(product.price)}
+          </Text>
+
+          <Text className="text-slate-400 font-body text-base leading-6 mb-6">
+            {product.description}
+          </Text>
+
+          {product.ingredients.map((ingredient) => (
+            <Text key={ingredient} className="text-slate-400 font-body text-base leading-6">
+              {"\u2022"} {" "} {ingredient}
+            </Text>
+          ))}
+        </ScrollView>
       </View>
 
       <View className="p-5 pb-8 gap-5">
